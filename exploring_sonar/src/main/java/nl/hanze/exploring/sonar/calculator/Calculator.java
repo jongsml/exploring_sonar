ce3a292f869f16479714528d954ea3d83e667d05
jongsml <leon.jongsma@ndcmediagroep.nl>
2015-04-13 13:32:30 +0200

    cleanup calculator

--- exploring_sonar/src/main/java/nl/hanze/exploring/sonar/calculator/Calculator.java
+++ exploring_sonar/src/main/java/nl/hanze/exploring/sonar/calculator/Calculator.java
@@ -4,12 +4,9 @@ import nl.hanze.exploring.sonar.calculator.contract.CalculatorContract;
 
 public class Calculator implements CalculatorContract
 {
-
 	@Override
 	public int sum(int a, int b) 
 	{
 		return a + b;		
 	}
-	
-
 }

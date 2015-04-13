2ade8a560be139805cb90effa37fbf7f73135199
jongsml <leon.jongsma@ndcmediagroep.nl>
2015-04-13 16:42:26 +0200

    pojo deleted

--- exploring_sonar/src/main/java/nl/hanze/exploring/sonar/file/FileListReader.java
+++ exploring_sonar/src/main/java/nl/hanze/exploring/sonar/file/FileListReader.java
@@ -10,18 +10,14 @@ import java.util.List;
 public class FileListReader extends AbstractFileReader implements FileReader {
   private static final Logger LOGGER = LogManager.getLogger(FileListReader.class.getName());
   private List<String> files;
-  private String dirname;
-  private String filename;
-
+  
   /** initialise reader with a directory name and a filename.
    * @param dirname input directory name
    * @param filename input file name
    */
   public FileListReader(String dirname, String filename) {
     try {
-      this.dirname = dirname;
-      this.filename = filename;
-      files = readFile(this.dirname, this.filename);
+      files = readFile(dirname, filename);
     } catch (IOException e) {
     LOGGER.log(Level.ERROR, e);
     }
@@ -30,23 +26,5 @@ public class FileListReader extends AbstractFileReader implements FileReader {
   @Override
   public final List<String> generateFileList() {
     return files;
-  }
-
-  public String getDirname() {
-    return dirname;
-  }
-
-  public void setDirname(String dirname) {
-    this.dirname = dirname;
-  }
-
-  public String getFilename() {
-    return filename;
-  }
-
-  public void setFilename(String filename) {
-    this.filename = filename;
-  }
-  
-  
+  }  
 }
